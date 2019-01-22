4502ac7c8328db6239e78a8b491295374899d309
yosef.wu <wurz529@foxmail.com>
2019-01-22 14:50:19 +0800

    fixed some bugs

---
+++ testcases/test_withdraw.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time     : 2019/1/22 13:50
+# @Author   : Yosef-夜雨声烦
+# @Email    : wurz529@foxmail.com
+# @File     : test_withdraw.py
+# @Software : PyCharm
